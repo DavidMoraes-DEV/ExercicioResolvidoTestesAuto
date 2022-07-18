500f12b7900fc1af8ed1c34fa6bfc8eda56994c3
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-07-18 18:48:36 -0300

    Projeto Base para iniciar o Exercicio Resolvido de testes automatizados do curso DevSuperior

---
+++ backend/src/main/java/com/ExercicioResolvidoTestesAuto/ExercicioResolvidoTestesAuto/ExercicioResolvidoTestesAutoApplication.java
@@ -0,0 +1,13 @@
+package com.ExercicioResolvidoTestesAuto.ExercicioResolvidoTestesAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioResolvidoTestesAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioResolvidoTestesAutoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ExercicioResolvidoTestesAuto/ExercicioResolvidoTestesAuto/ExercicioResolvidoTestesAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExercicioResolvidoTestesAuto.ExercicioResolvidoTestesAuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioResolvidoTestesAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
